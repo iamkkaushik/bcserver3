290f062952ec74d5c92edd8beadaa54f1c49a960
iamkkaushik <160194586+iamkkaushik@users.noreply.github.com>
2024-11-16 13:07:07 +0530

    Update4 server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "https://bhadracottage.onrender.com",
+    origin: "https://bhadracottage.vercel.app",
     methods: ["GET", "POST", "DELETE", "PUT"],
     // allowedHeaders: [
     //   "Content-Type",
